a86a26e82eede169620f1c37bd30b7ea03bc09a0
popcorefucktory <madforcarrie@gmail.com>
2017-06-13 17:29:26 +0300

    123

---
+++ app/src/androidTest/java/com/example/pm/myapplication/LoginActivityTest.java
@@ -0,0 +1,43 @@
+package com.example.pm.myapplication;
+
+
+import android.support.test.espresso.ViewInteraction;
+import android.support.test.rule.ActivityTestRule;
+import android.support.test.runner.AndroidJUnit4;
+import android.test.suitebuilder.annotation.LargeTest;
+
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+import static android.support.test.espresso.Espresso.onView;
+import static android.support.test.espresso.action.ViewActions.click;
+import static android.support.test.espresso.action.ViewActions.closeSoftKeyboard;
+import static android.support.test.espresso.action.ViewActions.replaceText;
+import static android.support.test.espresso.action.ViewActions.scrollTo;
+import static android.support.test.espresso.matcher.ViewMatchers.withId;
+
+@LargeTest
+@RunWith(AndroidJUnit4.class)
+public class LoginActivityTest {
+
+    @Rule
+    public ActivityTestRule<LoginActivity> mActivityTestRule = new ActivityTestRule<>(LoginActivity.class);
+
+    @Test
+    public void loginActivityTest() {
+        ViewInteraction appCompatAutoCompleteTextView = onView(
+                withId(R.id.email));
+        appCompatAutoCompleteTextView.perform(scrollTo(), click());
+
+        ViewInteraction appCompatAutoCompleteTextView2 = onView(
+                withId(R.id.email));
+        appCompatAutoCompleteTextView2.perform(scrollTo(), click());
+
+        ViewInteraction appCompatAutoCompleteTextView3 = onView(
+                withId(R.id.email));
+        appCompatAutoCompleteTextView3.perform(scrollTo(), replaceText("qwerty"), closeSoftKeyboard());
+
+    }
+
+}
